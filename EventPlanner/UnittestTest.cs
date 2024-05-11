ad0171cc2ee52f0a9ae98910b1671f06e94a36bf
Owen Arts <owen.arts@hotmail.com>
2024-05-11 21:04:00 +0200

    Attempt to add unit tests

---
+++ EventPlanner/UnittestTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Sandbox;
+
+namespace EventPlanner;
+
+public class UnittestTest
+    {
+        [Test]
+        public void PassingTest()
+        {
+            Assert.That(4, Calculator.Add(2,2));
+        }
+
+        [Test]
+        public void FailingTest()
+        {
+            Assert.That(true, 1, 2);
+        }
+    }
+}
