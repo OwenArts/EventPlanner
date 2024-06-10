b079bd57dcb8acdc06d1372f73954187e7d71757
Owen Arts <owen.arts@hotmail.com>
2024-06-10 13:43:56 +0200

    Update

--- EventPlanner/Data/IPlannerManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using EventPlanner.Data.AbstractClasses;
-
-namespace EventPlanner.Data;
-
-public interface IPlannerManager
-{
-    void ValidateFestival(Festival festival);
-    Festival PlanFestival(Festival festival);
-}
